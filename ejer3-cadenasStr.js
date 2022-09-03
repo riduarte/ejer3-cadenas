3f0fd8eed744a70028ffd1f269ee0a67ce7f5d79
Richard Duarte <riduarte88@gmail.com>
2022-09-03 16:12:03 +0200

    Add files via upload

---
+++ ejer3-cadenasStr.js
@@ -0,0 +1,9 @@
+let Nombre = "Richard" 
+let Apellido = "Duarte"
+let estudiante= `${Nombre} ${Apellido}`
+let estudianteMayus = estudiante.toLocaleUpperCase()
+let estudianteMinus = estudiante.toLocaleLowerCase()
+let numLetras = estudiante.length
+let inicioLetra = Nombre.charAt()
+let finLetra = Apellido.charAt(Apellido.length -1)
+let isNombre = estudiante.includes(Nombre)
